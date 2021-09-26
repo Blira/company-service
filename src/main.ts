06a9d3c75dedb7ec26c118a0c5f462edc1f87cd5
Lira <lira@pop-os.localdomain>
2021-09-26 19:47:26 -0300

    refactor: stop microsservice creation on bootstrap

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,8 @@
 import { NestFactory } from '@nestjs/core';
-import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  app.connectMicroservice<MicroserviceOptions>({
-    transport: Transport.KAFKA,
-    options: {
-      client: {
-        clientId: 'company-service',
-        brokers: ['localhost:9092'],
-      }
-    }
-  });
-
-  await app.startAllMicroservices();
   await app.listen(3000);
 }
 bootstrap();
